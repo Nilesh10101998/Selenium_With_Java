6b0ffdbb39b27aa2129b05cfaa80e56cf4cefbc3
Nilesh Kasane <nileshkasane98@gmail.com>
2025-02-14 18:45:01 +0530

    Get the data from the properties file

--- seleniumwebdriver/src/test/java/DataDrivenTest2/ReadingPropertiesFile.java
+++ seleniumwebdriver/src/test/java/DataDrivenTest2/ReadingPropertiesFile.java
@@ -2,7 +2,9 @@ package DataDrivenTest2;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Collection;
 import java.util.Properties;
+import java.util.Set;
 
 public class ReadingPropertiesFile {
 
@@ -18,7 +20,23 @@ public class ReadingPropertiesFile {
 		propertyobj.load(file);
 		
 		//Reading data from properties file
+		String url = propertyobj.getProperty("appurl");
+		String email = propertyobj.getProperty("email");
+		String pwd = propertyobj.getProperty("password");	
+		String orid = propertyobj.getProperty("orderid");	
+		String custid = propertyobj.getProperty("customerid");	
 		
+		System.out.println(url + " " + email + " " + pwd + " " + orid + " " + custid + " ");
+		
+		//Reading all the keys from the properties file
+		Set<String> keys = propertyobj.stringPropertyNames(); //capturing all keys from the properties file
+		System.out.println(keys);
+		
+		//Reading all the values from the properties file
+		Collection<Object> values = propertyobj.values();
+		System.out.println(values);
+		
+		file.close();
 		
 	}
 }
